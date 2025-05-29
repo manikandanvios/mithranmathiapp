496bc930f714a19ccde9adaef78ea299102357bc
GAVSMACBCP <gavsmacbcp@200411MAC0040.local>
2025-05-29 16:28:12 +0530

    code name changed

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   // This widget is the root of your application.
-  //Mani
+  //Mani testeinh
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -105,6 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
+            Text('mani tested'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
